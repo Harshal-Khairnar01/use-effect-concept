9c9956f0423bb70e663a43ea6c0a6c7f6ba37133
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-05-26 12:38:50 +0530

    another use-case and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, seterror] = useState(null);
+  // const [data, setData] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, seterror] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      if (!response.ok) {
-        throw new Error("response not okay");
-      }
-      const fetchData = await response.json();
-      setData(fetchData);
-    } catch (error) {
-      seterror(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/posts"
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error("response not okay");
+  //     }
+  //     const fetchData = await response.json();
+  //     setData(fetchData);
+  //   } catch (error) {
+  //     seterror(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <ul>
+      {/* <ul>
         {data.map((post) => {
           return <li key={post.id}>{post.title}</li>;
         })}
-      </ul>
+      </ul> */}
+
+      <Counter/>
     </div>
   );
 }
